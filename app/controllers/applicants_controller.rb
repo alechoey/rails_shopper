08e536f671791209227b884511a34b7060a4c1c5
Alec Hoey <alechoey@gmail.com>
2016-07-12 15:43:03 -0700

    Modal for signup working

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -4,7 +4,9 @@ class ApplicantsController < ApplicationController
   end
 
   def create
-    # your code here
+    @applicant = Applicant.new(initial_applicant_params)
+    @applicant.workflow_state = 'applied'
+    redirect_to applicant_path(@applicant)
   end
 
   def update
@@ -13,5 +15,25 @@ class ApplicantsController < ApplicationController
 
   def show
     # your code here
+  end
+
+  private
+  def find_applicant
+    @applicant = Applicant.find(params[:id])
+  end
+
+  def initial_applicant_params
+    params.require(:applicant).permit(
+      :email,
+      :first_name,
+      :last_name,
+      :region,
+      :phone,
+      :email,
+      :phone_type,
+      :source,
+      :over_21,
+      :reaason
+    )
   end
 end
