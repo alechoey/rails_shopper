ba01c4f10a643400343b41e8a3202585cd0c800a
Alec Hoey <alechoey@gmail.com>
2016-07-12 14:06:15 -0700

    Init commit

---
+++ app/controllers/applicants_controller.rb
@@ -0,0 +1,17 @@
+class ApplicantsController < ApplicationController
+  def new
+    @applicant = Applicant.new
+  end
+
+  def create
+    # your code here
+  end
+
+  def update
+    # your code here
+  end
+
+  def show
+    # your code here
+  end
+end
